3dd12c4700ef77a7b98108b22d43200859888c34
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2023-07-12 19:12:33 +0300

    Add custom card to display listings on home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import {Nunito} from "next/font/google"
-import './globals.css'
+import { Nunito } from "next/font/google";
+import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import ToasterProvider from "./providers/ToasterProvider";
@@ -9,18 +9,18 @@ import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone app',
-}
+  title: "Airbnb",
+  description: "Airbnb clone app",
+};
 
 const font = Nunito({
-  subsets:["latin"],
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const currentUser = await getCurrentUser();
 
@@ -28,13 +28,14 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <ToasterProvider/>
-          <RentModal/>
-          <LoginModal/>
-          <RegisterModal/>
-        <Navbar currentUser={currentUser}/>
+          <ToasterProvider />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}</body>
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
     </html>
-  )
+  );
 }
